1614700f9ed73f3e0b487c9f031189aad0379216
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-26 17:01:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 phrase == "I LOVE YOU GRANDMA!"
 
-if speak_to_grandma(phrase.downcase)
+if speak_to_grandma = #{phrase}.downcase
   puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma(phrase.uppercase)
+elsif speak_to_grandma =
   puts "NO, NOT SINCE 1938!"
 else speak_to_grandma(phrase)
   puts "I LOVE YOU TOO PUMPKIN!"
