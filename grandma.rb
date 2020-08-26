2e01e654684c20b4ebd806a3cf404efdb43c9984
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-26 17:43:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase.downcase
+  else phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase.capitalize
   end
 end
