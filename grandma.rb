40736cca05aaf3d50f8b65de3a11959f80804bd4
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-26 17:45:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
